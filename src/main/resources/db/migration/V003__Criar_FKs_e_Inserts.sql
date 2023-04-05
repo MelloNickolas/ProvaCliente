e44a9299baa41cf86911f95a97cb902fa442622b
MelloNickolas <nickolasphmello10@gmail.com>
2023-04-05 07:50:08 -0300

    Finish project

--- src/main/resources/db/migration/V003__Criar_FKs_e_Inserts.sql
+++ src/main/resources/db/migration/V003__Criar_FKs_e_Inserts.sql
@@ -4,3 +4,7 @@ insert into cliente(nomecliente) values ('Nickolas');
 insert into cliente(nomecliente) values ('Victor');
 insert into cliente(nomecliente) values ('Duda');
 insert into cliente(nomecliente) values ('Marcela');
+
+insert into contasreceber(valorconta, dataconta, idcliente) values ('1200', '2006-03-07', 1);
+insert into contasreceber(valorconta, dataconta, idcliente) values ('1323.50', '2023-06-27', 3);
+insert into contasreceber(valorconta, dataconta, idcliente) values ('1982.25', '2022-05-31', 2);
