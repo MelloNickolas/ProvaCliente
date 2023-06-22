1e56ea6cc5ac3167cf8bc98a37e1fc0eac842e59
MelloNickolas <nickolasphmello10@gmail.com>
2023-06-22 12:02:38 -0300

    Update ContasRecebrQuery

--- src/main/java/com/br/etec/nickolas/provacliente/repository/contasreceber/ContasReceberRepositoryQuery.java
+++ src/main/java/com/br/etec/nickolas/provacliente/repository/contasreceber/ContasReceberRepositoryQuery.java
@@ -1,12 +1,12 @@
 package com.br.etec.nickolas.provacliente.repository.contasreceber;
 
-import com.br.etec.nickolas.provacliente.model.ContasReceber;
+import com.br.etec.nickolas.provacliente.repository.Dtos.ContasReceberDto;
 import com.br.etec.nickolas.provacliente.repository.filter.ContasReceberFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ContasReceberRepositoryQuery {
 
-    public Page<ContasReceber> filtrar(ContasReceberFilter contasreceberfilter, Pageable pageable);
+    public Page<ContasReceberDto> filtrar(ContasReceberFilter contasreceberfilter, Pageable pageable);
 
 }
